7436284b7e1f3ec6d55e2de76f7c6933a6b7559a
Mh122012 <156209685+Mh122012@users.noreply.github.com>
2024-01-11 10:02:48 +0530

    Add files via upload

---
+++ Finally.py
@@ -0,0 +1,11 @@
+def Navin():
+    try:
+        gyugg
+        print("welcome to site")
+    except:
+        jg
+        msg="please check your login id or pw"
+        print("NameError::",msg)
+    finally:
+        print("thank you to visit")
+Navin()
